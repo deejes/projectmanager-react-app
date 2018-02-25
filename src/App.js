13e933005d7ff058d2039fc0ec7bc1654811e61b
deejes <j.devansh29@gmail.com>
2018-02-25 00:36:54 -0800

    render basic project information with projectItem component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import './App.css';
 import Projects from './Components/Projects'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+        projects: [
+            {
+                title: "Business Website",
+                category: "web Design"
+            },
+            {
+                title: "Social App",
+                category: "Mobile Dev"
+            },
+            {
+                title: "Ecommerce",
+                category: "web development"
+            }
+        ]
+
+
+    }
+}
+
   render() {
     return (
       <div className="App">
        This is my app 
-        <Projects />
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-
+import ProjectItem from './ProjectItem';
 class Projects extends Component {
   render() {
+    let projectItems;
+    if (this.props.projects){
+        projectItems = this.props.projects.map(project => {
+            return (<ProjectItem project={project}  />)
+        })
+    }     
+    console.log(this.props)
     return (
       <div className="Projects">
-       My Projects
+      {projectItems}
       </div>
     );
   }
